fe96024e9100f606761815f8b7a19eecc8d71bdb
Sabco <sabcobackup@gmail.com>
2025-05-16 02:47:27 +0530

    feat: a test document

---
+++ sabco/sabco/doctype/test_document/test_document.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Star Bottles and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test document", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sabco/sabco/doctype/test_document/test_document.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Star Bottles and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Testdocument(Document):
+	pass
